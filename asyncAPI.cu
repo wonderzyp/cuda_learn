c0edc31be644c8009354bc09f968e14180747416
wonderzyp <2671078523@qq.com>
2022-02-28 11:37:32 +0800

    cpu and gpu work together

--- asyncAPI.cu
+++ asyncAPI.cu
@@ -5,6 +5,21 @@
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 
+__global__ void d_fun(int *g_data, int inc_value){
+  int idx = threadIdx.x+blockDim.x*blockIdx.x;
+  g_data[idx] = g_data[idx]+inc_value;
+}
+
+bool correct_output(int *data, const int n, const int x) {
+  for (int i = 0; i < n; i++)
+    if (data[i] != x) {
+      printf("Error! data[%d] = %d, ref = %d\n", i, data[i], x);
+      return false;
+    }
+
+  return true;
+}
+
 
 int main()
 {
@@ -14,5 +29,57 @@ int main()
   checkCudaErrors(cudaGetDeviceProperties(&deviceProps, devID));
   printf("CUDA device [%s]\n", deviceProps.name);
 
+  int n = 16*1024*1024;
+  int nbytes = n*sizeof(int);
+  int value =26;
+  
+  // 分配内存
+  int *a = 0;
+  checkCudaErrors(cudaMallocHost((void**)&a, nbytes));
+  memset(a, 0, nbytes);
+
+  int *d_a=0;
+  checkCudaErrors(cudaMalloc((void**)&d_a, nbytes));
+  checkCudaErrors(cudaMemset(d_a, 255, nbytes));
+
+  dim3 threads = dim3(512, 1);
+  dim3 blocks = dim3(n/threads.x ,1);
+
+  cudaEvent_t start, stop;
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
+
+
+  checkCudaErrors(cudaDeviceSynchronize());
+  float gpu_time =0.0f;
+
+  cudaEventRecord(start,0);
+  cudaMemcpyAsync(d_a, a, nbytes, cudaMemcpyHostToDevice,0);
+  d_fun<<<blocks, threads,0,0>>> (d_a, value);
+  cudaMemcpyAsync(a, d_a, nbytes, cudaMemcpyDeviceToHost,0);
+  cudaEventRecord(stop, 0);
+
+// CPU可在GPU计算的同时进行工作
+  unsigned long int counter =0;
+  while (cudaEventQuery(stop)==cudaErrorNotReady){
+    ++counter;
+  }
+
+  checkCudaErrors(cudaEventElapsedTime(&gpu_time, start, stop));
+
+  printf("time spent executing by the GPU: %.2fms\n", gpu_time);
+  // printf("time spent by CPU in CUDA calls: %.2f\n", sdkGetTimerValue(&timer));
+  printf("CPU executed %lu iterations while waiting for GPU to finish\n",
+         counter);
+
+  bool checkResult = correct_output(a,n,value);
+
+  // release resources
+  checkCudaErrors(cudaEventDestroy(start));
+  checkCudaErrors(cudaEventDestroy(stop));
+  checkCudaErrors(cudaFreeHost(a));
+  checkCudaErrors(cudaFree(d_a));
+
+  exit(checkResult ? EXIT_SUCCESS : EXIT_FAILURE);
 }
 
